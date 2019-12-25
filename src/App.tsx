775755c09de8d79a62d67543f00ae5ff5288e8bd
sx <tanjiexiong@tungee.com>
2019-12-25 17:37:08 +0800

    添加eslint和prettier保证代码风格一致

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import BaseLayout from './layout/BaseLayout';
 import './App.css';
-import BaseLayout from "./layout/BaseLayout";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-        <BrowserRouter>
-            <Switch>
-                <Route path="/*" component={BaseLayout}/>
-            </Switch>
-        </BrowserRouter>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Switch>
+        <Route path="/*" component={BaseLayout} />
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- src/views/games/GamesView.tsx
+++ src/views/games/GamesView.tsx
@@ -1,119 +1,153 @@
 import React from "react";
-import {List, Input, Divider, Breadcrumb, Dropdown, Button, Pagination, Affix} from "antd";
-import GameCard, {Game} from "../../components/GameCard/GameCard";
+import {
+  List,
+  Input,
+  Divider,
+  Breadcrumb,
+  Dropdown,
+  Button,
+  Pagination,
+  Affix
+} from "antd";
+import GameCard, { Game } from "../../components/GameCard/GameCard";
 import GameService from "../../service/GameService";
-import {AxiosResponse} from "axios";
-import {RouteComponentProps} from "react-router";
-import {Link} from "react-router-dom";
+import { AxiosResponse } from "axios";
+import { RouteComponentProps } from "react-router";
+import { Link } from "react-router-dom";
 
 const { Search } = Input;
 
 interface GameListState {
-    games: Game[],
-    loading: boolean,
-    total: number,
-    page: number
+  games: Game[];
+  loading: boolean;
+  total: number;
+  page: number;
 }
 
 const emptyGame: Game = {
-    id: "",
-    nameCn: "",
-    platform: "",
-    platinum: 0,
-    gold: 0,
-    silver: 0,
-    bronze: 0,
-    iconUrl: "",
-    smallIconUrl: ""
+  id: "",
+  nameCn: "",
+  platform: "",
+  platinum: 0,
+  gold: 0,
+  silver: 0,
+  bronze: 0,
+  iconUrl: "",
+  smallIconUrl: ""
 };
 
 const service: GameService = new GameService();
 
-class GameView extends React.Component<RouteComponentProps, GameListState>{
+class GameView extends React.Component<RouteComponentProps, GameListState> {
+  state = {
+    games: new Array(24).fill(emptyGame),
+    loading: true,
+    total: 0,
+    page:
+      new URLSearchParams(this.props.location.search.substring(1)).get(
+        "page"
+      ) === null
+        ? 1
+        : Number(
+            new URLSearchParams(this.props.location.search.substring(1)).get(
+              "page"
+            )
+          )
+  };
 
-    state = {
-        games: new Array(24).fill(emptyGame),
-        loading: true,
-        total: 0,
-        page: new URLSearchParams(this.props.location.search.substring(1)).get("page") === null ?
-            1 : Number(new URLSearchParams(this.props.location.search.substring(1)).get("page"))
-    };
+  constructor(props: RouteComponentProps, state: GameListState) {
+    super(props, state);
+  }
 
-    constructor(props: RouteComponentProps, state: GameListState){
-        super(props, state);
-    }
+  getGameList = (pageNum: number) => {
+    this.setState({
+      games: new Array(24).fill(emptyGame),
+      loading: true,
+      total: 0,
+      page: this.state.page
+    });
+    service
+      .getGames(pageNum, 24)
+      .then((response: AxiosResponse) => {
+        if (response.data.status === 200) {
+          this.setState({
+            games: response.data.data.list,
+            loading: false,
+            total: response.data.data.total
+          });
+        } else {
+          this.handleGetGameListError();
+        }
+      })
+      .catch(error => {
+        this.handleGetGameListError();
+      });
+  };
 
-    getGameList = (pageNum: number) => {
-        this.setState({
-            games: new Array(24).fill(emptyGame),
-            loading: true,
-            total: 0,
-            page: this.state.page
-        });
-        service.getGames(pageNum, 24).then((response: AxiosResponse) => {
-            if(response.data.status === 200) {
-                this.setState({games:response.data.data.list, loading: false, total:response.data.data.total});
-            } else {
-                this.handleGetGameListError()
-            }
-        }).catch(error => {
-            this.handleGetGameListError()
-        })
-    };
+  handleGetGameListError = () => {
+    this.setState({ games: [], loading: false, total: 0 });
+  };
 
-    handleGetGameListError = () =>  {
-        this.setState({games: [], loading: false, total: 0})
-    };
+  componentWillMount = () => {
+    this.getGameList(this.state.page);
+  };
 
-    componentWillMount = () => {
-        this.getGameList(this.state.page);
-    };
+  pageChanged = (pageNum: number) => {
+    this.props.history.push(`?page=${pageNum}`);
+    this.scrollToTop();
+    this.getGameList(pageNum);
+  };
 
-    pageChanged = (pageNum : number) => {
-        this.props.history.push(`?page=${pageNum}`);
-        this.scrollToTop();
-        this.getGameList(pageNum);
-    };
+  scrollToTop = () => {
+    window.scrollTo({
+      left: 0,
+      top: 0,
+      behavior: "smooth"
+    });
+  };
 
-    scrollToTop = () =>{
-        window.scrollTo({
-            left: 0,
-            top: 0,
-            behavior: 'smooth',
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                <div className="ant-row-flex ant-row-flex-space-between" style={{width: "100%"}}>
-                    <Search
-                        enterButton
-                        placeholder="搜索游戏..."
-                        style={{width: "40%"}}
-                        onSearch={value => console.log(value)}
-                    />
-                    <Button icon="filter" type="primary">
-                        筛选
-                    </Button>
-                </div>
-                <Divider />
-                <List
-                    grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl:6}}
-                    dataSource={this.state == null ? [] : this.state.games}
-                    renderItem={game => (
-                        <List.Item>
-                            <Link to={`/games/${game.id}`}>
-                                <GameCard data={game} loading={this.state.loading}/>
-                            </Link>
-                        </List.Item>
-                    )}/>
-                <div className="ant-row-flex ant-row-flex-center">
-                    <Pagination hideOnSinglePage size="small" defaultCurrent={this.state.page} total={this.state.total} pageSize={24} onChange={this.pageChanged} />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div
+          className="ant-row-flex ant-row-flex-space-between"
+          style={{ width: "100%" }}
+        >
+          <Search
+            enterButton
+            placeholder="搜索游戏..."
+            style={{ width: "40%" }}
+            onSearch={value => console.log(value)}
+          />
+          <Button icon="filter" type="primary">
+            筛选
+          </Button>
+        </div>
+        <Divider />
+        <List
+          grid={{ gutter: 16, xs: 1, sm: 2, md: 2, lg: 3, xl: 4, xxl: 6 }}
+          dataSource={this.state == null ? [] : this.state.games}
+          renderItem={game => (
+            <List.Item>
+              <Link to={`/games/${game.id}`}>
+                <GameCard data={game} loading={this.state.loading} />
+              </Link>
+            </List.Item>
+          )}
+        />
+        <div className="ant-row-flex ant-row-flex-center">
+          <Pagination
+            hideOnSinglePage
+            size="small"
+            defaultCurrent={this.state.page}
+            total={this.state.total}
+            pageSize={24}
+            onChange={this.pageChanged}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GameView;
